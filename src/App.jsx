e1b5662b7329897d5ab2717b5221975a6bd9237d
Rohansaini1512 <2022kucp1050@iiitkota.ac.in>
2024-10-23 19:29:43 +0530

    Removing loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import Connect from './Pages/Connect';
 import Skills from './Pages/Skills';
 import Projects from './Pages/Projects';
-import Loading from './Components/Loading';
+// import Loading from './Components/Loading';
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000); // 6000 milliseconds = 6 seconds
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 3000); // 6000 milliseconds = 6 seconds
 
-    return () => clearTimeout(timer); // Clean up the timer if the component unmounts
-  }, []);
+  //   return () => clearTimeout(timer); // Clean up the timer if the component unmounts
+  // }, []);
 
   return (
     <>
-      {loading ? (
-        <Loading />
-      ) : (
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
@@ -30,9 +27,8 @@ function App() {
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
-      )}
     </>
-  );
+  )
 }
 
 export default App;
